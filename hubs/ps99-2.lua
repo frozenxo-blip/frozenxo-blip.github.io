88ed3aa7801e1bcee62cea014cc7cafe981d589e
frozenxo-blip <159326231+frozenxo-blip@users.noreply.github.com>
2024-04-17 17:44:37 +0100

    Update ps99-2.lua

--- hubs/ps99-2.lua
+++ hubs/ps99-2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/main/8e41737df5d1332dbdb7015c.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/main/6ac89313e162a853f49f8c51.lua'))()

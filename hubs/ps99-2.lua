d6d4ddbb7eb9a7aae1370ad7bb3d536b2fd1df38
frozenxo-blip <159326231+frozenxo-blip@users.noreply.github.com>
2024-04-17 17:56:36 +0100

    Update ps99-2.lua

--- hubs/ps99-2.lua
+++ hubs/ps99-2.lua
@@ -1,3 +1,3 @@
-print("Trying to Authenticate"
+print("Trying to Authenticate")
 loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/main/6ac89313e162a853f49f8c51.lua'))()
 print("Authentication Success")
